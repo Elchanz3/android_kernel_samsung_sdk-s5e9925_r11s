627b1cfce77f51950a9b5ded988cf3e970a46fcf
snapdragon teaks <inutilidades639@gmail.com>
2025-04-06 16:36:56 -0300

    Revert "fvmap: allow to read fvmap data"
    This reverts commit 46d3cf3e46bcc79ead28b315148c79527a71a777.

--- include/soc/samsung/cal-if.h
+++ include/soc/samsung/cal-if.h
@@ -18,9 +18,6 @@ static inline int cal_if_init(void)
 
 #define BLKPWR_MAGIC	0xB1380000
 
-#define READ_VOLT 0
-#define READ_RATE 1
-
 extern int (*exynos_cal_pd_sci_sync)(unsigned int id, bool on);
 extern int (*exynos_cal_pd_bcm_sync)(unsigned int id, bool on);
 extern void (*exynos_cal_pd_bts_sync)(unsigned int id, int on);
@@ -134,8 +131,4 @@ extern void cal_register_pd_lookup_cmu_id(void *(*func)(u32 cmu_id));
 #define cal_vclk_dbg_info(a)	do{} while(0);
 //extern void cal_vclk_dbg_info(unsigned int id);
 #endif
-
-int fvmap_patch(unsigned int dvfs_type, unsigned int rate, unsigned int volt);
-unsigned int fvmap_read(unsigned int dvfs_type, int mode, unsigned int value);
-
 #endif
