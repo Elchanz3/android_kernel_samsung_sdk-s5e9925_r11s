9bd12d0a1ff3607747e6b933d08d7a603fe69a1e
snapdragon teaks <inutilidades639@gmail.com>
2025-04-06 15:30:13 -0300

    fvmap: optimize voltages
    org commit : xxmustafacooTR/exynos-linux-stable@4a41ebd

--- drivers/soc/samsung/cal-if/fvmap.c
+++ drivers/soc/samsung/cal-if/fvmap.c
@@ -262,6 +262,32 @@ static const struct attribute_group percent_margin_group = {
 	.attrs = percent_margin_attrs,
 };
 
+static void optimize_rate_volt_table(struct rate_volt_header *head, unsigned int num_of_lv) {
+ 	bool changed;
+ 	int i;
+ 
+ 	/* optimize voltages */
+ 	while (true) {
+ 		changed = false;
+ 
+ 		for (i = 1; i < num_of_lv; i++) {
+ 			/* switch voltages if higher frequency uses less */
+ 			if (head->table[i].volt > head->table[i-1].volt) {
+ 				int temp_volt = head->table[i-1].volt;
+ 
+ 				head->table[i-1].volt = head->table[i].volt;
+ 				head->table[i].volt = temp_volt;
+ 
+ 				changed = true;
+ 			}
+ 		}
+ 
+ 		if (!changed)
+ 			break;
+ 	}
+ }
+ 
+
 unsigned int dvfs_calibrate_voltage(unsigned int rate_target, unsigned int rate_up,
 		unsigned int rate_down, unsigned int volt_up, unsigned int volt_down)
 {
@@ -429,6 +455,8 @@ static void fvmap_copy_from_sram(void __iomem *map_base, void __iomem *sram_base
 #endif
 		old = sram_base + fvmap_header[i].o_ratevolt;
 		new = map_base + fvmap_header[i].o_ratevolt;
+		
+		optimize_rate_volt_table(old, fvmap_header[i].num_of_lv);
 
 		margin = init_margin_table[vclk->margin_id];
 		if (margin)
